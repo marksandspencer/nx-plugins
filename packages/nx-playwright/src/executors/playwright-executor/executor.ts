25f5c0d5ce61a07e8ea6bd9d2145232eaf32f77d
Prabhat Thapa <prabhat4ever@gmail.com>
2022-06-01 11:17:55 +0100

    test secret scan

--- packages/nx-playwright/src/executors/playwright-executor/executor.ts
+++ packages/nx-playwright/src/executors/playwright-executor/executor.ts
@@ -9,6 +9,7 @@ export default async function executor(
   context: ExecutorContext,
 ) {
   let success: boolean;
+  const TEAMS_WEBHOOK="https://mnscorp.webhook.office.com/webhookb2/64e209ff-e52d-45c9-9223-153db5765718@bd5c6713-7399-4b31-be79-78f2d078e543/IncomingWebhook/48dbd7596cb94192b94cbad39f4ffb4b/9f90c474-7541-4f38-a971-d6cec0ca63be"
   await startDevServer(options, context);
   try {
     const { stdout, stderr } = await promisify(exec)(
