cd1df122f90502fa690c304da306124817ff8c2a
Andrea Caldera <andrea.caldera@gmail.com>
2022-06-10 13:13:09 +0100

    move version check to ts

---
+++ ci/version-check.ts
@@ -0,0 +1,14 @@
+const { execSync } = require('child_process');
+
+const { version } = require(`${process.cwd()}/packages/nx-playwright/package.json`);
+
+console.log(`Current version ${version}`);
+
+const releases: string[] = execSync('git tag').toString().split('\n').filter(Boolean);
+
+console.log('Existing releases', releases);
+
+if (releases.includes(version)) {
+  console.error(`Version ${version} already released, please update nx-playwright version`);
+  process.exit(1);
+}
