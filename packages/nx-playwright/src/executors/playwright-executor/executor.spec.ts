ff3d05ac9c286d4b067752a0363a89fa999b74de
Nate Kidwell <ludicast@users.noreply.github.com>
2022-08-17 07:48:44 -0400

    add coverage test

--- packages/nx-playwright/src/executors/playwright-executor/executor.spec.ts
+++ packages/nx-playwright/src/executors/playwright-executor/executor.spec.ts
@@ -1,5 +1,6 @@
 import * as utilModule from 'util';
 import executor from './executor';
+import { PlaywrightExecutorSchema } from './schema';
 import * as startDevServerModule from './lib/start-dev-server';
 
 const startDevServer = jest
@@ -14,57 +15,80 @@ const context = {
   cwd: '',
 };
 
-const options = {
-  e2eFolder: 'folder',
-};
-
 console.error = jest.fn().mockReturnValue(null);
 console.info = jest.fn().mockReturnValue(null);
 
 describe('executor', () => {
   beforeEach(jest.resetAllMocks);
 
-  it('returns true when passes', async () => {
-    promisify.mockReturnValueOnce(
-      jest.fn().mockResolvedValueOnce({ stdout: 'passed', stderr: '' }),
-    );
+  describe("with cli opts", () => {
+    const options: PlaywrightExecutorSchema = {
+      e2eFolder: 'folder',
+      headed: true,
+      browser: "firefox",
+      reporter: "html"
+    };
 
-    const { success } = await executor(options, context);
+    it("concatenates options to playwright command", async () => {
+      const execCmd = jest.fn().mockResolvedValueOnce({ stdout: 'passed', stderr: '' });
+      promisify.mockReturnValueOnce(
+        execCmd
+      );
 
-    expect(success).toBe(true);
+      await executor(options, context);
 
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.info).toHaveBeenCalledTimes(1);
-    expect(console.info).toHaveBeenCalledWith('Playwright output passed');
-
-    expect(startDevServer).toHaveBeenCalledTimes(1);
-    expect(startDevServer).toHaveBeenCalledWith(options, context);
+      let expected = "yarn playwright test src --config folder/playwright.config.ts --headed --browser=firefox --reporter=html";
+      expect(execCmd).toHaveBeenCalledWith(expected);
+    });
   });
 
-  it('logs error from output', async () => {
-    promisify.mockReturnValueOnce(
-      jest.fn().mockResolvedValueOnce({ stdout: 'passed', stderr: 'some error' }),
-    );
+  describe("playwright execution", () => {
+    const options: PlaywrightExecutorSchema = {
+      e2eFolder: 'folder',
+    };
 
-    const { success } = await executor(options, context);
+    it('returns true when passes', async () => {
+      promisify.mockReturnValueOnce(
+        jest.fn().mockResolvedValueOnce({ stdout: 'passed', stderr: '' }),
+      );
 
-    expect(success).toBe(true);
+      const { success } = await executor(options, context);
 
-    expect(console.error).toHaveBeenCalledTimes(1);
-    expect(console.error).toHaveBeenCalledWith('Playwright errors some error');
-    expect(console.info).toHaveBeenCalledTimes(1);
-    expect(console.info).toHaveBeenCalledWith('Playwright output passed');
-  });
+      expect(success).toBe(true);
 
-  it('fails gracefully when command fails', async () => {
-    const error = new Error('fake error');
-    promisify.mockReturnValueOnce(jest.fn().mockRejectedValueOnce(error));
+      expect(console.error).not.toHaveBeenCalled();
+      expect(console.info).toHaveBeenCalledTimes(1);
+      expect(console.info).toHaveBeenCalledWith('Playwright output passed');
 
-    const { success } = await executor(options, context);
+      expect(startDevServer).toHaveBeenCalledTimes(1);
+      expect(startDevServer).toHaveBeenCalledWith(options, context);
+    });
 
-    expect(success).toBe(false);
+    it('logs error from output', async () => {
+      promisify.mockReturnValueOnce(
+        jest.fn().mockResolvedValueOnce({ stdout: 'passed', stderr: 'some error' }),
+      );
 
-    expect(console.error).toHaveBeenCalledTimes(1);
-    expect(console.error).toHaveBeenCalledWith('Unexpected error', error);
-  });
+      const { success } = await executor(options, context);
+
+      expect(success).toBe(true);
+
+      expect(console.error).toHaveBeenCalledTimes(1);
+      expect(console.error).toHaveBeenCalledWith('Playwright errors some error');
+      expect(console.info).toHaveBeenCalledTimes(1);
+      expect(console.info).toHaveBeenCalledWith('Playwright output passed');
+    });
+
+    it('fails gracefully when command fails', async () => {
+      const error = new Error('fake error');
+      promisify.mockReturnValueOnce(jest.fn().mockRejectedValueOnce(error));
+
+      const { success } = await executor(options, context);
+
+      expect(success).toBe(false);
+
+      expect(console.error).toHaveBeenCalledTimes(1);
+      expect(console.error).toHaveBeenCalledWith('Unexpected error', error);
+    });
+  })
 });
