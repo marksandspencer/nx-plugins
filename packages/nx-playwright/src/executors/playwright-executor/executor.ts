01c5420aa50851463a255b353712c63473162880
Nate Kidwell <ludicast@users.noreply.github.com>
2022-08-20 09:37:41 -0400

    updated values

--- packages/nx-playwright/src/executors/playwright-executor/executor.ts
+++ packages/nx-playwright/src/executors/playwright-executor/executor.ts
@@ -4,6 +4,19 @@ import { promisify } from 'util';
 import { startDevServer } from './lib/start-dev-server';
 import { PlaywrightExecutorSchema } from './schema';
 
+function getFlags(options: PlaywrightExecutorSchema): string {
+  const headedOption = options.headed === true ? '--headed' : '';
+  const browserOption = options.browser?.length ? `--browser=${options.browser}` : '';
+  const reporterOption = options.reporter?.length ? `--reporter=${options.reporter}` : '';
+  const timeoutOution = (options.timeout ?? `--timeout=${options.timeout}`) || '';
+
+  const flagStrings = [
+    headedOption, browserOption, reporterOption, timeoutOution
+  ].filter(Boolean);
+
+  return flagStrings.join(' ');
+}
+
 export default async function executor(
   options: PlaywrightExecutorSchema,
   context: ExecutorContext,
@@ -12,9 +25,7 @@ export default async function executor(
 
   const success = await Promise.resolve()
     .then(async () => {
-      const headedOption = options.headed === true ? '--headed' : '';
-      const browserOption = options.browser?.length ? `--browser=${options.browser}` : '';
-      const reporterOption = options.reporter?.length ? `--reporter=${options.reporter}` : '';
+
 
       const { stdout, stderr } = await promisify(exec)(
         `yarn playwright test src --config ${options.e2eFolder}/playwright.config.ts ${headedOption} ${browserOption} ${reporterOption}`,
