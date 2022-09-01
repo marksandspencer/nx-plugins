0c8b8bf798c4b72e31275d91900842c7812f84a3
Nate Kidwell <ludicast@users.noreply.github.com>
2022-09-01 05:42:15 -0400

    Update packages/nx-playwright/src/generators/init/lib/add-git-ignore-entry.ts
    Co-authored-by: Andrea Caldera <andrea.caldera@gmail.com>

--- packages/nx-playwright/src/generators/init/lib/add-git-ignore-entry.ts
+++ packages/nx-playwright/src/generators/init/lib/add-git-ignore-entry.ts
@@ -1,6 +1,25 @@
 import { Tree } from '@nrwl/devkit';
 import ignore from 'ignore';
 
+export const addGitIgnoreEntry = (host: Tree) => {
+  const content = host.exists('.gitignore') ? host.read('.gitignore', 'utf-8').trimEnd() : '';
+
+  const ig = ignore();
+  ig.add(host.read('.gitignore', 'utf-8'));
+
+  const addIgnore = (existingIgnore: string, ignoreToAdd: string) => {
+    return ig.ignores(existingIgnore) ? '' : ignoreToAdd;
+  };
+
+  const resultsIgnore = addIgnore('app/example/test-results', `\n\n# Playwright\n**/test-results/`);
+  const reportsIgnore = addIgnore('app/example/playwright-report', `\n**/playwright-report/`);
+  const cacheIgnore = addIgnore('app/example/playwright/.cache', '\n**/playwright/.cache/\n');
+
+  host.write('.gitignore', `${content}${resultsIgnore}${reportsIgnore}${cacheIgnore}`);
+};
+
+import ignore from 'ignore';
+
 export function addGitIgnoreEntry(host: Tree) {
   if (!host.exists('.gitignore')) {
     return;
