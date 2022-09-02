7b6404974b147ce513cbfa9db1d29dc305138f91
Nate Kidwell <ludicast@users.noreply.github.com>
2022-09-02 04:45:40 -0400

    incorporate suggestions

--- packages/nx-playwright/src/generators/init/lib/add-git-ignore-entry.ts
+++ packages/nx-playwright/src/generators/init/lib/add-git-ignore-entry.ts
@@ -1,27 +1,19 @@
 import { Tree } from '@nrwl/devkit';
 import ignore from 'ignore';
 
-export function addGitIgnoreEntry(host: Tree) {
-  if (!host.exists('.gitignore')) {
-    return;
-  }
-
-  let content = host.read('.gitignore', 'utf-8').trimEnd();
-
+export const addGitIgnoreEntry = (host: Tree) => {
+  const content = host.exists('.gitignore') ? host.read('.gitignore', 'utf-8').trimEnd() : '';
+  
   const ig = ignore();
   ig.add(host.read('.gitignore', 'utf-8'));
+  
+  const addIgnore = (existingIgnore: string, ignoreToAdd: string) => {
+    return ig.ignores(existingIgnore) ? '' : ignoreToAdd;
+  };
 
-  if (!ig.ignores('app/example/test-results')) {
-    content = `${content}\n\n# Playwright\n**/test-results/`;
-  }
-
-  if (!ig.ignores('app/example/playwright-report')) {
-    content = `${content}\n**/playwright-report/`;
-  }
-
-  if (!ig.ignores('app/example/playwright/.cache')) {
-    content = `${content}\n**/playwright/.cache/\n`;
-  }
-
-  host.write('.gitignore', content);
-}
+  const resultsIgnore = addIgnore('app/example/test-results', `\n\n# Playwright\n**/test-results/`);
+  const reportsIgnore = addIgnore('app/example/playwright-report', `\n**/playwright-report/`);
+  const cacheIgnore = addIgnore('app/example/playwright/.cache', '\n**/playwright/.cache/\n');
+  
+  host.write('.gitignore', `${content}${resultsIgnore}${reportsIgnore}${cacheIgnore}`);
+};
