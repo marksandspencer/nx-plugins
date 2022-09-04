5e90193a06c907c0044e5a8f103d1891ea554488
Nate Kidwell <ludicast@users.noreply.github.com>
2022-09-03 20:58:36 -0400

    Remove spaces

--- packages/nx-playwright/src/generators/init/lib/add-git-ignore-entry.ts
+++ packages/nx-playwright/src/generators/init/lib/add-git-ignore-entry.ts
@@ -3,10 +3,10 @@ import ignore from 'ignore';
 
 export const addGitIgnoreEntry = (host: Tree) => {
   const content = host.exists('.gitignore') ? host.read('.gitignore', 'utf-8').trimEnd() : '';
-  
+
   const ig = ignore();
   ig.add(host.read('.gitignore', 'utf-8'));
-  
+
   const addIgnore = (existingIgnore: string, ignoreToAdd: string) => {
     return ig.ignores(existingIgnore) ? '' : ignoreToAdd;
   };
@@ -14,6 +14,6 @@ export const addGitIgnoreEntry = (host: Tree) => {
   const resultsIgnore = addIgnore('app/example/test-results', `\n\n# Playwright\n**/test-results/`);
   const reportsIgnore = addIgnore('app/example/playwright-report', `\n**/playwright-report/`);
   const cacheIgnore = addIgnore('app/example/playwright/.cache', '\n**/playwright/.cache/\n');
-  
+
   host.write('.gitignore', `${content}${resultsIgnore}${reportsIgnore}${cacheIgnore}`);
 };
