313dd19484fab439500f203c4d430aa8c07b8fc8
Andrea Caldera <andrea.caldera@gmail.com>
2023-01-27 15:13:41 +0000

    fix

--- packages/nx-playwright/src/executors/playwright-executor/executor.ts
+++ packages/nx-playwright/src/executors/playwright-executor/executor.ts
@@ -8,7 +8,7 @@ function getFlags(options: PlaywrightExecutorSchema) {
   const headedOption = options.headed === true ? '--headed' : '';
   const passWithNoTestsOption = options.passWithNoTests === true ? '--pass-with-no-tests' : '';
   const browserOption = options.browser?.length ? `--browser=${options.browser}` : '';
-  const projectOption = options.testProject?.length ? `--project=${options.proj}` : '';
+  const projectOption = options.testProject?.length ? `--project=${options.testProject}` : '';
   const reporterOption = options.reporter?.length ? `--reporter=${options.reporter}` : '';
   const timeoutOption = options.timeout !== undefined ? `--timeout=${options.timeout}` : '';
   const grepOption = options.grep !== undefined ? `--grep=${options.grep}` : '';
