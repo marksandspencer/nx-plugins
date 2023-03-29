0735930e4e16efd585975bf58604778a4a58c07f
Andrea Caldera <andrea.caldera@gmail.com>
2023-03-29 11:02:40 +0100

    fix path

--- local-test.sh
+++ local-test.sh
@@ -46,10 +46,13 @@ function restore_workspace_if_requested {
     fi
 }
 
+CURRENT_DIR=${pwd}
+
 PLUGIN_NPM_NAME="@mands/$PLUGIN_NAME"
 
 rm -fr dist
 pnpm nx build $PLUGIN_NAME
+
 
 pushd dist/packages/$PLUGIN_NAME
 pnpm link .
@@ -58,7 +61,7 @@ popd
 pushd $workspace
 stash_workspace_changes_if_requested
 pnpm unlink $PLUGIN_NPM_NAME
-pnpm link $PLUGIN_NPM_NAME --dir dist/packages/$PLUGIN_NAME
+pnpm link $PLUGIN_NPM_NAME --dir ${CURRENT_DIR}/dist/packages/$PLUGIN_NAME
 pnpm nx generate $PLUGIN_NPM_NAME:project $app-e2e --project $app
 pnpm nx e2e $app-e2e --skip-nx-cache
 restore_workspace_if_requested
