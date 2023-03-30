a11841a500486a15d9272e18ba8e8b2ae7c7d08a
Andrea Caldera <andrea.caldera@gmail.com>
2023-03-30 10:05:50 +0100

    improve version checks

--- ci/version-check.ts
+++ ci/version-check.ts
@@ -1,14 +1,30 @@
-const { execSync } = require('child_process');
+import fetch from 'isomorphic-unfetch';
 
 const { version } = require(`${process.cwd()}/packages/nx-playwright/package.json`);
 
 console.log(`Current version ${version}`);
 
-const releases: string[] = execSync('git tag').toString().split('\n').filter(Boolean);
+const validateVersion = async () => {
+  const response = await fetch(`https://api.github.com/repos/marksandspencer/nx-plugins/tags`);
+  if (response.status >= 400) {
+    return Promise.reject(`Invalid response for tag request ${response.status}`);
+  }
+  const data = await response.json();
+  const releases = data.map(({ name }: { name: string }) => name);
+  console.log('Existing releases', releases);
+  if (!releases.length || releases.includes(version)) {
+    return Promise.reject(
+      `Version ${version} has already been released, please update nx-playwright version`,
+    );
+  }
+};
 
-console.log('Existing releases', releases);
-
-if (!releases.length || releases.includes(version)) {
-  console.error(`Version ${version} already released, please update nx-playwright version`);
-  process.exit(1);
-}
+validateVersion()
+  .then(() => {
+    console.info(`Version ${version} is valid`);
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
