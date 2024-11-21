dc7fce23ab118dad24217934d5141957ab7a35e6
Judit Greskovits <329441+juditgreskovits@users.noreply.github.com>
2024-11-21 14:48:32 +0000

    chore: add bash script to add missing target

---
+++ add-serve-target.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e -x
+
+PARAMETER_VALIDATION_PROMPT="
+Please supply the following arguments:
+
+  -w <path to workspace>: absolute path to the workspace with which to test the plugin
+  -a <app name>: the name of the app with which to test the plugin
+  
+For example: 
+  ./add-serve-target.sh -w path/to/workspace -a example-app"
+
+while getopts w:a: flag
+do
+    case "${flag}" in
+        w) workspace=${OPTARG};;
+        a) app=${OPTARG};;
+    esac
+done
+
+if [ -z $workspace ] || [ -z $app ]; then
+    echo "$PARAMETER_VALIDATION_PROMPT"
+    exit 1
+fi
+
+# Path to the JSON file
+NX_PROJECT_FILE=$workspace/apps/$app/project.json
+
+# Check if the file exists
+if [[ ! -f "$NX_PROJECT_FILE" ]]; then
+  echo "Error: File $NX_PROJECT_FILE does not exist."
+  exit 1
+fi
+
+# Load the JSON file and add a new section
+NEW_NX_PROJECT=$(jq '.target += {"serve": {"executor": "@nx/web:file-server", "options": { "buildTarget": "build", "staticFilePath": "apps/test-app/out", "port": 3000, "spa": false}}}' "$NX_PROJECT_FILE")
+
+# Check if jq succeeded
+if [[ $? -ne 0 ]]; then
+  echo "Error: Failed to process JSON with jq."
+  exit 1
+fi
+
+# Save the modified JSON back to the original file
+echo "$NEW_NX_PROJECT" > "$NX_PROJECT_FILE"
+
+echo "Successfully updated $NX_PROJECT_FILE."
